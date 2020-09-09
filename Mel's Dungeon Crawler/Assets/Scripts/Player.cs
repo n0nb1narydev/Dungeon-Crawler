72f55ac08c8e90a3f7039a15ba7ba31d63ddd31a
Mel Rumsey <melsrumsey@gmail.com>
2020-09-09 16:09:14 -0500

    char and anim

---
+++ Mel's Dungeon Crawler/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Player : MonoBehaviour
+{
+    private NavMeshAgent nma;
+    private Animator anim;
+    private bool isRunning = false;
+    void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                nma.destination = hit.point;
+            }
+        }
+
+        if(nma.remainingDistance <= nma.stoppingDistance)
+        {
+            isRunning = false;
+        } 
+        else
+        {
+            isRunning = true;
+        }
+
+        anim.SetBool("isRunning", isRunning);
+    }
+
+}
